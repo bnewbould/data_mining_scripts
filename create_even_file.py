07c8e12a392c826b2bbcb927025152da6072dd32
bnewbould <benjamin.newbould@my.jcu.edu.au>
2019-06-03 21:18:41 +1000

    Update create_even_file.py

--- create_even_file.py
+++ create_even_file.py
@@ -1,22 +1,37 @@
-def main():
-    f = open('testSet.txt', errors="replace")
-    lines = f.readlines()
-    fout = open('even_set.txt', 'w')
-    pos = 0
-    neg = 0
-    for line in lines:
-        if line[-2] == '0':
-            if neg < 5000:
-                neg+=1
-                fout.write(line)
-        else:
-            if pos < 5000:
-                pos+=1
-                fout.write(line)
-        if pos >= 5000 and neg >= 5000:
-            print('it\'s all ogre')
-            break
-        
+import csv
 
+class BreakoutException(Exception):
+    pass
+
+def main():
+    with open('files/clean_fixed_set.txt', errors="replace") as file:
+        lines = file.readlines()
+        file_out = open('files/even_clean_set.txt', 'w', newline='')
+        pos = 0
+        neg = 0
+        output = []
+        csvwriter = csv.writer(file_out,quotechar='\'', delimiter=',', quoting=csv.QUOTE_NONNUMERIC)
+        for att in csv.reader(lines, quotechar='\'', delimiter=','):
+            try:
+                if att[6] == '0':
+                    if neg < 5000:
+                        neg+=1
+                        raise BreakoutException
+                else:
+                    if pos < 5000:
+                        pos+=1
+                        raise BreakoutException
+            except BreakoutException:
+                try:
+                    csvwriter.writerow(att)
+                except:
+                    print("reeeeee")
+                    continue
+            if pos >= 5000 and neg >= 5000:
+                print('it\'s all ogre')
+                break
+        file_out.close()
+        file.close()
+        
 if __name__ == "__main__":
     main()
